008bbe1e3f28860d145109da62e510ebe63a3fd7
JavaCommons Technologies <javacommons@gmail.com>
2026-03-05 21:05:16 +0900

    # Commit at 2026.0305.2105.15 [https://github.com/npm-module/xp-lisp] [DIFF] diff --git a/.gitignore b/.gitignore     -#/npm-module     +/npm-module diff --git a/npm-module/.npmignore b/npm-module/.npmignore     [FILE DELETED] diff --git a/npm-module/LICENSE b/npm-module/LICENSE     [FILE DELETED] diff --git a/npm-module/README.md b/npm-module/README.md     [FILE DELETED] diff --git a/npm-module/esm/_dnt.shims.d.ts b/npm-module/esm/_dnt.shims.d.ts     [FILE DELETED] diff --git a/npm-module/esm/_dnt.shims.d.ts.map b/npm-module/esm/_dnt.shims.d.ts.map     [FILE DELETED] diff --git a/npm-module/esm/_dnt.shims.js b/npm-module/esm/_dnt.shims.js     [FILE DELETED] diff --git a/npm-module/esm/oml2ast.d.ts b/npm-module/esm/oml2ast.d.ts     [FILE DELETED] diff --git a/npm-module/esm/oml2ast.d.ts.map b/npm-module/esm/oml2ast.d.ts.map     [FILE DELETED] diff --git a/npm-module/esm/oml2ast.js b/npm-module/esm/oml2ast.js     [FILE DELETED] diff --git a/npm-module/esm/omlcommon.d.ts b/npm-module/esm/omlcommon.d.ts     [FILE DELETED] diff --git a/npm-module/esm/omlcommon.d.ts.map b/npm-module/esm/omlcommon.d.ts.map     [FILE DELETED] diff --git a/npm-module/esm/omlcommon.js b/npm-module/esm/omlcommon.js     [FILE DELETED] diff --git a/npm-module/esm/package.json b/npm-module/esm/package.json     [FILE DELETED] diff --git a/npm-module/esm/xp-lisp.d.ts b/npm-module/esm/xp-lisp.d.ts     [FILE DELETED] diff --git a/npm-module/esm/xp-lisp.d.ts.map b/npm-module/esm/xp-lisp.d.ts.map     [FILE DELETED] diff --git a/npm-module/esm/xp-lisp.js b/npm-module/esm/xp-lisp.js     [FILE DELETED] diff --git a/npm-module/package.json b/npm-module/package.json     [FILE DELETED] diff --git a/npm-module/script/_dnt.shims.d.ts b/npm-module/script/_dnt.shims.d.ts     [FILE DELETED] diff --git a/npm-module/script/_dnt.shims.d.ts.map b/npm-module/script/_dnt.shims.d.ts.map     [FILE DELETED] diff --git a/npm-module/script/_dnt.shims.js b/npm-module/script/_dnt.shims.js     [FILE DELETED] diff --git a/npm-module/script/oml2ast.d.ts b/npm-module/script/oml2ast.d.ts     [FILE DELETED] diff --git a/npm-module/script/oml2ast.d.ts.map b/npm-module/script/oml2ast.d.ts.map     [FILE DELETED] diff --git a/npm-module/script/oml2ast.js b/npm-module/script/oml2ast.js     [FILE DELETED] diff --git a/npm-module/script/omlcommon.d.ts b/npm-module/script/omlcommon.d.ts     [FILE DELETED] diff --git a/npm-module/script/omlcommon.d.ts.map b/npm-module/script/omlcommon.d.ts.map     [FILE DELETED] diff --git a/npm-module/script/omlcommon.js b/npm-module/script/omlcommon.js     [FILE DELETED] diff --git a/npm-module/script/package.json b/npm-module/script/package.json     [FILE DELETED] diff --git a/npm-module/script/xp-lisp.d.ts b/npm-module/script/xp-lisp.d.ts     [FILE DELETED] diff --git a/npm-module/script/xp-lisp.d.ts.map b/npm-module/script/xp-lisp.d.ts.map     [FILE DELETED] diff --git a/npm-module/script/xp-lisp.js b/npm-module/script/xp-lisp.js     [FILE DELETED] diff --git a/npm-module/src/_dnt.shims.ts b/npm-module/src/_dnt.shims.ts     [FILE DELETED] diff --git a/npm-module/src/oml2ast.js b/npm-module/src/oml2ast.js     [FILE DELETED] diff --git a/npm-module/src/omlcommon.js b/npm-module/src/omlcommon.js     [FILE DELETED] diff --git a/npm-module/src/xp-lisp.js b/npm-module/src/xp-lisp.js     [FILE DELETED] diff --git a/npm-module/test_runner.js b/npm-module/test_runner.js     [FILE DELETED] [/DIFF]

--- npm-module/script/xp-lisp.d.ts
+++
@@ -1,5 +0,0 @@
-export function version(): string;
-export function xpLisp($g: any): any;
-export function run(exp: any): any;
-export function runAll(exp: any): any;
-//# sourceMappingURL=xp-lisp.d.ts.map
